aadf88e71265d22dc2e8282feb64764c564eb764
Thibault D'ARCHIVIO <tdarchiv@student.42.fr>
2018-06-01 20:56:34 +0200

    Fixed memzero for > 4 elem size

--- libft/ft_memzero.c
+++ libft/ft_memzero.c
@@ -10,7 +10,7 @@ void	ft_memzero(void *src, register size_t elem_count, size_t elem_size)
 {
 	register int *ptr;
 
-	if (elem_size < 4)
+	if (elem_size != 4)
 		return ft_memzero_byte(src, elem_count * elem_size);
 	ptr = src;
 	while (--elem_count)
