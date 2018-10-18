856bf134d474322c5b9117d4520438af09d692af
Thibault D'ARCHIVIO <tdarchiv@student.42.fr>
2018-10-18 15:07:10 +0200

    NORMING DONE ZOMG

--- mlx_gui/mlx_gui.c
+++ mlx_gui/mlx_gui.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_gui.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdarchiv <tdarchiv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/18 15:05:11 by tdarchiv          #+#    #+#             */
+/*   Updated: 2018/10/18 15:06:01 by tdarchiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mlx_gui.h"
 
 #include <stdio.h>
@@ -10,7 +22,6 @@ int				expose_callback(void *param)
 	t_mlx_context *ctx;
 
 	ctx = param;
-	printf("Expose\n");
 	mlx_put_image_to_window(ctx->mlx_ptr, ctx->win_ptr, ctx->texture, 0, 0);
 	return (0);
 }
@@ -23,7 +34,6 @@ int				callback(void *param)
 	mlx_clear_window(ctx->mlx_ptr, ctx->win_ptr);
 	renderer_update(&ctx->renderer);
 	renderer_draw(ctx->renderer);
-//	renderer_draw0(ctx->renderer);
 	mlx_put_image_to_window(ctx->mlx_ptr, ctx->win_ptr, ctx->texture, 0, 0);
 	return (0);
 }
@@ -64,9 +74,7 @@ void			init_mlx(t_mlx_context *ctx, t_map map)
 	ctx->win_ptr = mlx_new_window(ctx->mlx_ptr, size.x, size.y, "Wireframe");
 	ctx->texture = mlx_new_image(ctx->mlx_ptr, size.x, size.y);
 	pixels = mlx_get_data_addr(ctx->texture, &osef, &osef, &osef);
-
 	renderer_init(&ctx->renderer, pixels, map, size);
-
 	mlx_do_key_autorepeatoff(ctx->mlx_ptr);
 	mlx_hook(ctx->win_ptr, 2, osef, keydown_event, ctx);
 	mlx_hook(ctx->win_ptr, 3, osef, keyup_event, ctx);
@@ -74,7 +82,5 @@ void			init_mlx(t_mlx_context *ctx, t_map map)
 	mlx_hook(ctx->win_ptr, 17, osef, quit_event, NULL);
 	mlx_expose_hook(ctx->win_ptr, expose_callback, ctx);
 	mlx_loop_hook(ctx->mlx_ptr, callback, ctx);
-//	mlx_mouse_hook(ctx->win_ptr, mouse_move, ctx);
 	mlx_loop(ctx->mlx_ptr);
 }
-
